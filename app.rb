6383d4cf90b2089c1116abe0a6bfc10a6e2bd069
John Britton <public@johndbritton.com>
2011-02-15 02:48:48 -0500

    Added Entrant class

--- app.rb
+++ app.rb
@@ -3,6 +3,25 @@ require 'sinatra'
 require 'builder'
 require 'rest_client'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'])
+
+class Entrant
+  include DataMapper::Resource
+
+  property :number, String, :required => True
+
+  validates_uniqueness_of :number
+end
+
+helpers do
+  def add_entrant(number)
+    e = Entrant.new
+    e.number = number
+    e.save
+  end
+end
+
 post '/voice' do
+  params['From']
   builder :welcome
 end
